2059f9a41bc816dcb31a41108aa75f2d7a6c4914
Jose Cupertino Ruiz Vargas <jruizvar@cern.ch>
2016-05-23 23:01:27 +0200

    Set lumiWeight==1 for signal

--- EDBRTreeMaker/signals/submitAll.sh
+++ EDBRTreeMaker/signals/submitAll.sh
@@ -10,10 +10,7 @@ sed -i '20 s/1600/1800/' submitJob.sh; bsub -q 1nh -M 2000 -J M1800mu < submitJo
 sed -i '20 s/1800/2000/' submitJob.sh; bsub -q 1nh -M 2000 -J M2000mu < submitJob.sh
 sed -i '20 s/2000/2500/' submitJob.sh; bsub -q 1nh -M 2000 -J M2500mu < submitJob.sh
 sed -i '20 s/2500/3000/' submitJob.sh; bsub -q 1nh -M 2000 -J M3000mu < submitJob.sh
-sed -i '20 s/3000/3500/' submitJob.sh; bsub -q 1nh -M 2000 -J M3500mu < submitJob.sh
-sed -i '20 s/3500/4000/' submitJob.sh; bsub -q 1nh -M 2000 -J M4000mu < submitJob.sh
-sed -i '20 s/4000/4500/' submitJob.sh; bsub -q 1nh -M 2000 -J M4500mu < submitJob.sh
-sed -i '20 s/4500/800/'  submitJob.sh; bsub -q 1nh -M 2000 -J  M800mu < submitJob.sh
+sed -i '20 s/3000/800/'  submitJob.sh; bsub -q 1nh -M 2000 -J  M800mu < submitJob.sh
 
 sed -i '20 s/mu/el/' submitJob.sh
 
@@ -25,9 +22,6 @@ sed -i '20 s/1600/1800/' submitJob.sh; bsub -q 1nh -M 2000 -J M1800el < submitJo
 sed -i '20 s/1800/2000/' submitJob.sh; bsub -q 1nh -M 2000 -J M2000el < submitJob.sh
 sed -i '20 s/2000/2500/' submitJob.sh; bsub -q 1nh -M 2000 -J M2500el < submitJob.sh
 sed -i '20 s/2500/3000/' submitJob.sh; bsub -q 1nh -M 2000 -J M3000el < submitJob.sh
-sed -i '20 s/3000/3500/' submitJob.sh; bsub -q 1nh -M 2000 -J M3500el < submitJob.sh
-sed -i '20 s/3500/4000/' submitJob.sh; bsub -q 1nh -M 2000 -J M4000el < submitJob.sh
-sed -i '20 s/4000/4500/' submitJob.sh; bsub -q 1nh -M 2000 -J M4500el < submitJob.sh
-sed -i '20 s/4500/800/'  submitJob.sh; bsub -q 1nh -M 2000 -J  M800el < submitJob.sh
+sed -i '20 s/3000/800/'  submitJob.sh; bsub -q 1nh -M 2000 -J  M800el < submitJob.sh
 
 sed -i '20 s/el/mu/' submitJob.sh
